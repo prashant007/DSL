2607292c911785bf3ee6c1da335f9f1f85a77b22
Martin Erwig <erwig@eecs.oregonstate.edu>
2020-09-16 13:04:07 -0700

    Update Car.hs

--- Car.hs
+++ Car.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE  DeriveAnyClass, LambdaCase #-}
+{-# LANGUAGE DeriveAnyClass #-}
 module Car where
 
 import qualified Data.Map.Strict as M
@@ -14,46 +14,30 @@ import Transformation
 
 -- Car Example
 --
-data User    = Friend | Expert deriving (Eq,Ord,Show,Enum,Bounded,Set)
 data Car     = Honda | BMW | Toyota deriving (Eq,Ord,Show,Enum,Bounded,Set)
 data Feature = Price | MPG | Safety deriving (Eq,Ord,Show,Enum,Bounded,Set)
+data Opinion = Friend | Expert deriving (Eq,Ord,Show,Enum,Bounded,Set)
 data Weight  = Weight deriving (Eq,Ord,Show,Enum,Bounded,Set)
 
 instance Valence Feature where
   valence Price = False
   valence _     = True
 
-instance Valence User
+instance Valence Opinion
 instance Valence Weight
 
 
--- (1) Mapping cars to features
+-- (1) Mapping carFeatures to opinions
 --
+carFeatures :: Info Car Feature
+carFeatures = info [Honda --> [Price --> 34000, MPG --> 30, Safety --> 9.8],
+                    BMW   --> [Price --> 36000, MPG --> 33, Safety --> 9.1]]
 
-{-
--- Use this when exploring realistic values:
---
-(c1N,c1P,c1M,c1S) = (Honda,34000,30,9.8)
-(c2N,c2P,c2M,c2S) = (BMW,36000,33,9.1)
 
--- values :: Feature -> Spread Car
-values :: Feature -> [(Car,Double)]
-values Price  = [Honda --> 34000, BMW --> 36000]
-values MPG    = [Honda --> 30,    BMW --> 33]
-values Safety = [Honda --> 9.8,   BMW --> 9.1]
-
-cars :: Info Car Feature
-cars = gather values
--}
-
-cars :: Info Car Feature
-cars = info [Honda --> [Price --> 34000, MPG --> 30, Safety --> 9.8],
-             BMW   --> [Price --> 36000, MPG --> 33, Safety --> 9.1]]
-
--- (2) creating a valuation from data (only for features)
+-- (2) creating a valuation from data (only for opinions)
 --
 carsV :: Val Car Feature
-carsV = valuation cars
+carsV = valuation carFeatures
 
 
 -- (3) Some variation: adding/deleting/modifying a feature attribute
@@ -64,12 +48,10 @@ toyota Safety = 9.4
 toyota MPG    = 30
 
 threeCars :: Info Car Feature
-threeCars = addAlternative Toyota toyota cars
+threeCars = addAlternative Toyota toyota carFeatures
 
-
-features3 = delAttribute cars Price
-
-features4 = modAttribute cars Price Honda 45000
+opinions3 = delAttribute carFeatures Price
+opinions4 = modAttribute carFeatures Price Honda 45000
 
 
 -- (3) Weighing attributes
@@ -82,18 +64,15 @@ carsVW :: Val Car (Feature,Weight)
 carsVW = mkOneTuple carsV `extendBy` fWeights
 
 
--- (4) Adding dimensions to car features (users and weights)
+-- (4) Adding dimensions to car opinions (opinions and weights)
 --
-userInfo :: User -> Spread Feature
-userInfo x = case x of Friend -> [Price --> 0.5, MPG  --> 0.3, Safety --> 0.2]
-                       Expert -> [Price --> 0.2, MPG  --> 0.4, Safety --> 0.4]
+featureOpinions :: Info Feature Opinion
+featureOpinions = info [Price  --> [Friend --> 0.5, Expert --> 0.2],
+                        MPG    --> [Friend --> 0.3, Expert --> 0.4],
+                        Safety --> [Friend --> 0.2, Expert --> 0.4]]
 
-users :: Info Feature User
-users = gather userInfo
-
-weights :: Info User Weight
+weights :: Info Opinion Weight
 weights = addAttribute Weight [Friend --> 0.6,Expert --> 0.4] objects
-
 
 
 bRec :: Rec Feature
@@ -102,27 +81,26 @@ bRec = mkRec [Price --> 36000, MPG  --> 24, Safety --> 70]
 bInfo :: Info Car Feature
 bInfo = mkInfo [BMW --> bRec]
 
+
 -- (5) Creating valuations for extended data
 --
--- carsUF :: Val Car (User,Feature)
--- carsUF = extend cars users
+-- carsUF :: Val Car (Opinion,Feature)
+-- carsUF = extend carFeatures featureOpinions
 --
--- carsWUF :: Val Car (Weight,User,Feature)
+-- carsWUF :: Val Car (Weight,Opinion,Feature)
 -- carsWUF = extend carsUF weights
 
-cars' :: Val Car (Feature,User,Weight)
--- cars = val cars `extendBy` users `extendBy` weights
-cars' = mkOneTuple carsV `extendBy` users `extendBy` weights
+cars :: Val Car (Feature,Opinion,Weight)
+-- carFeatures = val carFeatures `extendBy` featureOpinions `extendBy` weights
+cars = mkOneTuple carsV `extendBy` featureOpinions `extendBy` weights
 
 carPriority :: Priority Car
-carPriority = priority cars'
+carPriority = priority cars
 
--- ======================= FILTERING ELEMENTS FROM ANNOTATED VALUES ===========================
--- ============================================================================================
 
 -- (6) Explaining decisions
 --
-type CarDecomp = Norm (Feature,User,Weight)
+type CarDecomp = Norm (Feature,Opinion,Weight)
 
 -- Valuations for specific cars
 --
@@ -133,29 +111,29 @@ type CarDecomp = Norm (Feature,User,Weight)
 -- bmw = select BMW cars
 --
 vdCar :: CarDecomp
-vdCar = select Honda cars' `diff` select BMW cars'
+vdCar = select Honda cars `diff` select BMW cars
 
-exp0 :: Explain (Feature,User,Weight)
+exp0 :: Explain (Feature,Opinion,Weight)
 exp0 = explain vdCar
 
 
-explCar :: Explain (Feature,User,Weight)
+explCar :: Explain (Feature,Opinion,Weight)
 explCar = explain vdCar
 
 
 showMDSexp0 = pmds exp0
 showDomexp0 = pdom exp0
 
-exp1 :: Explain User
+exp1 :: Explain Opinion
 exp1 = generalize vdCar
 
 exp2 :: Explain Feature
 exp2 = generalize vdCar
 
-mds0 :: MDS (Feature,User)
+mds0 :: MDS (Feature,Opinion)
 mds0 = let (_,_,_,ms,_) = exp0
-       in  reduce (head ms)::Norm (Feature,User)
+       in  reduce (head ms)::Norm (Feature,Opinion)
 
-m01 = pFact (factorize mds0 :: Factor Feature User)
-m02 = pFact (factorize mds0 :: Factor User Feature)
--- m11 = factorize mds1 :: Factor Weight (User,Feature)
+m01 = pFact (factorize mds0 :: Factor Feature Opinion)
+m02 = pFact (factorize mds0 :: Factor Opinion Feature)
+-- m11 = factorize mds1 :: Factor Weight (Opinion,Feature)
